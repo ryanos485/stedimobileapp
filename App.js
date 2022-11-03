1bbc2722d92f20441f6f57f77594419b89a429ba
Ryan Osborne <osb21006@byui.edum>
2022-11-03 15:02:43 -0600

    made changes to phonenumber

--- App.js
+++ App.js
@@ -1,120 +1,287 @@
 import React, { useEffect, useState, } from 'react';
-import { StyleSheet, Text, View, Image, TouchableOpacity, AsyncStorage, TextInput, Button } from 'react-native';
+
+import { StyleSheet, Text, View, Image, TouchableOpacity, } from 'react-native';
+
 import  Navigation from './components/Navigation';
+
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 import OnboardingScreen from './screens/OnboardingScreen';
+
 import Home from './screens/Home';
+
 import { NavigationContainer } from '@react-navigation/native';
 
+import { Button, TextInput } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+//Just a comment for push
 
-
+ 
 
 const AppStack = createNativeStackNavigator();
-const LoggedInState={
+
+const loggedInStates = {
+
   NOT_LOGGED_IN: 'NOT_LOGGED_IN',
+
   LOGGED_IN: 'LOGGED_IN',
+
   CODE_SENT: 'CODE_SENT'
+
 }
+
+ 
 
 const App = () =>{
+
   const [isFirstLaunch, setFirstLaunch] = React.useState(true);
-  const [loggedInState,setLoggedInState] = React.useState(loggedInStates.NOT_LOGGED_IN);
+
+  const [loggedInState, setLoggedInState] = React.useState(loggedInStates.NOT_LOGGED_IN);
+
   const [homeTodayScore, setHomeTodayScore] = React.useState(0);
-  const [phoneNumber, setPhoneNumber] = React.useState("")
-  const [oneTimePassword, SetOnceTimePassword] = React.useState(null);
+
+  const[phoneNumber, setPhoneNumber] = React.useState('');
+
+  const [oneTimePassword, setOneTimePassword] = React.useState(null)
+
+ 
 
    if (isFirstLaunch == true){
-return(
-  <OnboardingScreen setFirstLaunch={setFirstLaunch}/>
- 
-);
-  }else if(loggedInState == loggedInStates.LOGGED_IN){
-    return <Navigation/>
-  } else if(loggedInState == loggedInState.NOT_LOGGED_IN){
-    return(
-      <View>
-        <TextInput style={styles.input}
-        placeholderTextColor='#4251f5'
-        placeholder='Phone Number'
-        value={phoneNumber}
-        onChangeText={setPhoneNumber}
-        >
-        </TextInput>
-        <Button
-        title='login'
-        style={styles.button}
-        onPress={async()=>{
-          console.log('Login Button was pressed!')
-          await fetch('https://dev.stedi.me/twofactorlogin',
-          {method:'post', headers:
-        {'content-type':'application/text'}body})
-        setLoggedInState(loggedInStates.CODE_SENT)
-        }}
-        />
-        </View>
-    )
-  }else if(loggedInState == loggedInStates.CODE_SENT) {
-    
-    return (
-    <View>
-      
-      <TextInput style={styles.input}
-        placeholderTextColor='#4251f5'
-        placeholder='Phone Number'
-        value={phoneNumber}
-        onChangeText={setOneTimePassword}
-        keyboardType = "numeric"
-        >
-        </TextInput>
-        <Button
-        title='send'
-        style={styles.button}
-        onPress={async()=>{
-          console.log('Button was pressed!')
-          await fetch('https://dev.stedi.me/twofactorlogin/'+phoneNumber,
-          {method:'post', headers:
-        {'content-type':'application/text'}})
-        setLoggedInState(loggedInStates.CODE_SENT)
-        }}
-        />
-        </View>
-    )
-  }else if(loggedInState == loggedInStates.CODE_SENT) {
-    
-    return (
-    <View>
-      
-      <TextInput style={styles.input}
-        placeholderTextColor='#4251f5'
-        placeholder='Phone Number'
-        value={phoneNumber}
-        onChangeText={setOneTimePassword}
-        keyboardType = "numeric"
-        ></TextInput>
 
-    </View>
-   
-    )}
-}
+return(
+
+  <OnboardingScreen setFirstLaunch={setFirstLaunch}/>
+
+ 
+
+);
+
+  }else if(loggedInState == loggedInStates.LOGGED_IN){
+
+    return <Navigation/>
+
+  } else if(loggedInState == loggedInStates.NOT_LOGGED_IN){
+
+    return(<View>
+
+            <Text style={{fontWeight:'bold', textAlign:'center', color:'#A0CE4E', fontSize:19, margin:12,  marginTop: -70}}>Your Privacy is safe with us!
+
+              Please enter your phone number.</Text>
+
+            <TextInput placeholder='Phone number'
+
+             style={{fontWeight:'bold',
+
+                    textAlign:'center',
+
+                    color:'#5059c9',
+
+                    height: 40,
+
+                    margin: 12,
+
+                    borderWidth: 2,
+
+                    borderRadius: 5,
+
+                    margin: 0,
+
+                    marginTop: 100,
+
+                    fontSize:18,
+
+                    backgroundColor:"#ffbd4f"
+
+                   
+
+                    }}
+
+                    value = {phoneNumber}
+
+                    onChangeText = {setPhoneNumber}>
+
+              </TextInput>
+
+            <Button title='Send'
+
+                    style={{fontWeight:'bold', textAlign:'center', color:'#A0CE4E', fontSize:30, margin:12,  marginTop: 400, backgroundColor:'#0b0b0b', margin: 30,
+
+                        borderWidth: 2,
+
+                       borderRadius: 5,}}
+
+                    onPress = {async()=>{
+
+                    console.log('This Button was pressed!');
+
+                    var the_url = await fetch("https://dev.stedi.me/twofactorlogin/" + phoneNumber,
+
+                    {
+
+                      method: 'POST',
+
+                      headers:{
+
+                        'content-type': 'application/text'
+
+                      }
+
+ 
+
+                    })
+
+                    setLoggedInState(loggedInStates.CODE_SENT)
+
+                    }}>
+
+                    </Button>
+
+              </View>
+
+              )}
+
+              else if(loggedInState == loggedInStates.CODE_SENT){
+
+                return(
+
+                  <View>
+
+                    <TextInput placeholder='One Time Password'
+
+             style={{fontWeight:'bold',
+
+                    textAlign:'center',
+
+                    color:'#5059c9',
+
+                    height: 40,
+
+                    margin: 12,
+
+                    borderWidth: 2,
+
+                    borderRadius: 5,
+
+                    margin: 0,
+
+                    marginTop: 100,
+
+                    }}
+
+                    value = {oneTimePassword}
+
+                    onChangeText = {setOneTimePassword}
+
+                    keyboardType = "numeric">
+
+              </TextInput>
+
+              <Button title='Login'
+
+                    style={{fontWeight:'bold', textAlign:'center', color:'#A0CE4E', fontSize:19, margin:12,  marginTop: 400, backgroundColor:'black', margin: 12,
+
+                        borderWidth: 2,
+
+                       borderRadius: 5,}}
+
+                    onPress = {async()=>{
+
+                    console.log('Login Button was pressed!');
+
+                    const loginResponse = await fetch("https://dev.stedi.me/twofactorlogin",
+
+                    {
+
+                      method: 'POST',
+
+                      headers:{
+
+                        'content-type': 'application/text'
+
+                      },
+
+                      body:JSON.stringify({
+
+                        phoneNumber,
+
+                        oneTimePassword
+
+                      })
+
+ 
+
+                    })
+                    console.log("loginresponsestatus",loginResponse.status)
+
+                    if(loginResponse.status == 200){
+
+                      setLoggedInState(loggedInStates.LOGGED_IN)
+
+                    }else{
+
+                      setLoggedInState(loggedInStates.NOT_LOGGED_IN)
+
+                    }
+
+                    }}>
+
+                    </Button>
+
+                  </View>
+
+                )
+
+              }
+
+            }
+
  export default App;
- const styles = styles.create({
+
+ 
+
+//Where we are
+
+ 
+
+const styles = StyleSheet.create({
+
   container:{
-      flex:1, 
+
+      flex:1,
+
       alignItems:'center',
+
       justifyContent: 'center'
+
   },
+
   input: {
+
     height: 40,
-    margin: 12,marginTop: 100,
+
+    margin: 12,
+
     borderWidth: 1,
+
     padding: 10,
+
   },
+
   margin:{
+
     marginTop:100
+
   },
-  button: {
+
+  Button: {
+
     alignItems: "center",
+
     backgroundColor: "#DDDDDD",
+
     padding: 10
+
   }    
+
 })
+
